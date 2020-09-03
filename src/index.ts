bc0f4cf5d39ada473cdb432178559c670799b1bd
Patrik Marin <dev@patrikmarin.fi>
2020-09-03 20:09:25 +0300

    Init migration script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-import {dynamo} from '@aws-sdk/client-dynamodb-v2-node';
+import {Config as AWSConfig} from 'aws-sdk/lib/config';
+import {SharedIniFileCredentials} from 'aws-sdk/lib/credentials/shared_ini_file_credentials';
+
+import {DocumentClient} from 'aws-sdk/clients/dynamodb';
+
+interface Configuration {
+  region: string;
+  profile: string;
+}
+
+const sourceConfiguration = {
+  region: 'eu-central-1',
+  profile: 'patrikmarin',
+};
+
+const tableName = 'ReminderSubscriptions';
+
+const config = new AWSConfig();
+
+const {region, profile} = sourceConfiguration;
+
+config.update({
+  region,
+  credentials: new SharedIniFileCredentials({profile}),
+});
+
+(async () => {
+  const client = new DocumentClient();
+  const {Items, Count, LastEvaluatedKey} = await client
+    .scan({
+      TableName: tableName,
+    })
+    .promise();
+
+  console.log(Count, LastEvaluatedKey, Items);
+})();
