5c457829b50645ca354a969e43941db793d42549
Patrik Marin <dev@patrikmarin.fi>
2020-09-03 21:15:31 +0300

    Add table creation

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,141 @@
 import * as AWS from 'aws-sdk';
 
+import {IndexName, KeySchema, Projection} from 'aws-sdk/clients/dynamodb';
+
 interface Configuration {
   region: string;
   profile: string;
 }
 
-const sourceConfiguration = {
+const sourceConfiguration: Configuration = {
+  region: 'eu-central-1',
+  profile: 'patrikmarin',
+};
+
+const targetConfiguration: Configuration = {
   region: 'eu-central-1',
   profile: 'patrikmarin',
 };
 
 const tableName = 'ReminderSubscriptions';
 
-const {region, profile} = sourceConfiguration;
+const configureAWS = (type: 'source' | 'local' | 'target') => {
+  switch (type) {
+    case 'source':
+      return AWS.config.update({
+        region: sourceConfiguration.region,
+        credentials: new AWS.SharedIniFileCredentials({
+          profile: sourceConfiguration.profile,
+        }),
+        accessKeyId: undefined,
+        secretAccessKey: undefined,
+      });
+    case 'target':
+      return AWS.config.update({
+        region: targetConfiguration.region,
+        credentials: new AWS.SharedIniFileCredentials({
+          profile: targetConfiguration.profile,
+        }),
+        accessKeyId: undefined,
+        secretAccessKey: undefined,
+      });
+    case 'local':
+      return AWS.config.update({
+        region: 'local',
+        credentials: undefined,
+        accessKeyId: 'local',
+        secretAccessKey: 'local',
+      });
+  }
+};
 
-AWS.config.update({
-  region,
-  credentials: new AWS.SharedIniFileCredentials({profile}),
-});
+type RequiredIndexType = {
+  IndexName: IndexName;
+  KeySchema: KeySchema;
+  Projection: Projection;
+};
+
+const parseIndexResponse = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  indexes: undefined | Array<Partial<RequiredIndexType> & {[x: string]: any}>
+): RequiredIndexType[] | undefined => {
+  return indexes
+    ? indexes.map(({IndexName, KeySchema, Projection}) => {
+        if (!IndexName || !KeySchema || !Projection) {
+          throw new Error('Index invalid!');
+        }
+        return {
+          IndexName,
+          KeySchema,
+          Projection,
+        };
+      })
+    : undefined;
+};
 
 (async () => {
-  const client = new AWS.DynamoDB.DocumentClient();
+  // 1. DescribeTable
+  configureAWS('source');
+  const sourceClient = new AWS.DynamoDB();
+
+  const {
+    Table: tableDescription,
+    $response: descriptionResponse,
+  } = await sourceClient
+    .describeTable({
+      TableName: tableName,
+    })
+    .promise();
+
+  if (!tableDescription) {
+    console.info(descriptionResponse.data);
+    if (descriptionResponse.error) {
+      console.error(descriptionResponse.error);
+    }
+    return;
+  }
+
+  if (!tableDescription.AttributeDefinitions || !tableDescription.KeySchema) {
+    console.error('No attribute definitions for table!');
+    return;
+  }
+
+  configureAWS('local');
+  const localClient = new AWS.DynamoDB({
+    endpoint: 'http://localhost:8000',
+  });
+
+  const {
+    TableDescription: createdTableDescription,
+    $response: createResponse,
+  } = await localClient
+    .createTable({
+      AttributeDefinitions: tableDescription.AttributeDefinitions,
+      TableName: 'dynamodb-migrator-temporary-table',
+      KeySchema: tableDescription.KeySchema,
+      LocalSecondaryIndexes: parseIndexResponse(
+        tableDescription.LocalSecondaryIndexes
+      ),
+      GlobalSecondaryIndexes: parseIndexResponse(
+        tableDescription.GlobalSecondaryIndexes
+      ),
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 40000,
+        WriteCapacityUnits: 40000,
+      },
+    })
+    .promise();
+
+  if (!createdTableDescription) {
+    console.info(createResponse.data);
+    if (createResponse.error) {
+      console.error(createResponse.error);
+    }
+    return;
+  }
+
+  console.log('Table created with name', createdTableDescription.TableName);
+  /*
   const {Items, Count, LastEvaluatedKey} = await client
     .scan({
       TableName: tableName,
@@ -28,4 +143,6 @@ AWS.config.update({
     .promise();
 
   console.log(Count, LastEvaluatedKey, Items);
+  configureAWS('local');
+  */
 })();
