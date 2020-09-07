38e88aa679329737877d212ab826dfefc431f590
Patrik Marin <dev@patrikmarin.fi>
2020-09-07 20:50:35 +0300

    Add test base and use stream

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,17 @@
-import {getDynamoDB} from './config';
+import through2 from 'through2';
 import {convertDescriptionToInput} from './utils';
-import DynamoDB, {Key} from 'aws-sdk/clients/dynamodb';
 
-const tableName = 'ReminderSubscriptions';
+import {
+  deleteTemporaryTable,
+  describeSourceTable,
+  createTemporaryTable,
+  getItemsFromSourceTable,
+} from './operations';
+import {DynamoDB} from 'aws-sdk';
+
 const temporaryTableName = 'dynamodb-migrator-temporary-table';
 
-const deleteTemporaryTable = async (): Promise<void> =>
-  new Promise((resolve, reject) =>
-    getDynamoDB('local').deleteTable({TableName: temporaryTableName}, err => {
-      if (err && err.code !== 'ResourceNotFoundException') {
-        console.error(err);
-        return reject(new Error('Temporary table deletion failed!'));
-      }
-      console.log('Deleted temporary table', temporaryTableName);
-      return resolve();
-    })
-  );
-
-const describeSourceTable = async (): Promise<AWS.DynamoDB.TableDescription> =>
-  new Promise((resolve, reject) =>
-    getDynamoDB('source').describeTable(
-      {TableName: tableName},
-      (err, {Table}) => {
-        if (err) {
-          console.error(err);
-          return reject(new Error('Temporary table deletion failed!'));
-        }
-        if (!Table) {
-          return reject(
-            new Error('Failed to get description for source table!')
-          );
-        }
-        console.log('Fetched description for table', tableName);
-        return resolve(Table);
-      }
-    )
-  );
-
-const createTemporaryTable = async (
-  tableInput: AWS.DynamoDB.CreateTableInput
-): Promise<void> =>
-  new Promise((resolve, reject) =>
-    getDynamoDB('local').createTable(tableInput, (err, {TableDescription}) => {
-      if (!TableDescription || err) {
-        console.error(err);
-        return reject(new Error('Failed to create table'));
-      }
-      console.log('Created temporary table', TableDescription.TableName);
-      return resolve();
-    })
-  );
-
-const getItemsFromSourceTable = async (
-  items: DynamoDB.AttributeMap[],
-  startKey: Key | undefined = undefined
-): Promise<DynamoDB.ItemList> =>
-  new Promise((resolve, reject) =>
-    getDynamoDB('source').scan(
-      {
-        TableName: tableName,
-        ExclusiveStartKey: startKey,
-      },
-      (err, {Items, Count, LastEvaluatedKey}) => {
-        if (err) {
-          console.error(err);
-          return reject(new Error('Failed to get items from table'));
-        }
-
-        if (!Items || !Count) {
-          return reject(new Error('Table did not contain any items'));
-        }
-
-        console.log(
-          'Fetched',
-          Count,
-          'items (total',
-          Count + items.length,
-          'items)'
-        );
-        const combinedItems = items.concat(...Items);
-        if (LastEvaluatedKey !== undefined) {
-          console.log('Table read was partial, fetching more data');
-          return getItemsFromSourceTable(combinedItems, LastEvaluatedKey);
-        }
-        return resolve(combinedItems);
-      }
-    )
-  );
-
-(async () => {
+export const main = async () => {
   try {
     // 0. Delete local temporary table
     await deleteTemporaryTable();
@@ -104,12 +27,30 @@ const getItemsFromSourceTable = async (
     // 2. Create temporary table from source table description
     await createTemporaryTable(tableInput);
 
-    // 3. Get items from source table
-    const sourceItems = await getItemsFromSourceTable([]);
-    console.log(sourceItems);
+    // Create stream to handle reading / writing
+    const itemStream = through2({objectMode: true});
+
+    // Start listening stream input
+    itemStream.on('data', (chunk: DynamoDB.ItemList) => {
+      console.log(chunk);
+    });
+
+    itemStream.on('error', e => {
+      console.log(e);
+    });
+
+    itemStream.on('end', () => {
+      console.log('end');
+    });
+
+    // 3. Get items from source table and add them to stream
+    await getItemsFromSourceTable(itemStream);
+
     return 0;
   } catch (e) {
     console.error(e);
     return -1;
   }
-})();
+};
+
+main();
---
+++ src/operations.ts
@@ -0,0 +1,112 @@
+import DynamoDB, {Key} from 'aws-sdk/clients/dynamodb';
+import {Transform} from 'stream';
+import {getDynamoDB} from './config';
+
+const tableName = 'ReminderSubscriptions';
+const temporaryTableName = 'dynamodb-migrator-temporary-table';
+
+export const deleteTemporaryTable = async (): Promise<void> =>
+  new Promise((resolve, reject) =>
+    getDynamoDB('local').deleteTable({TableName: temporaryTableName}, err => {
+      if (err && err.code !== 'ResourceNotFoundException') {
+        console.error(err);
+        return reject(new Error('Temporary table deletion failed!'));
+      }
+      console.log('Deleted temporary table', temporaryTableName);
+      return resolve();
+    })
+  );
+
+export const describeSourceTable = async (): Promise<
+  AWS.DynamoDB.TableDescription
+> =>
+  new Promise((resolve, reject) =>
+    getDynamoDB('source').describeTable(
+      {TableName: tableName},
+      (err, {Table}) => {
+        if (err) {
+          console.error(err);
+          return reject(new Error('Failed to describe source table'));
+        }
+        if (!Table) {
+          return reject(
+            new Error('Failed to get description for source table!')
+          );
+        }
+        console.log('Fetched description for table', tableName);
+        return resolve(Table);
+      }
+    )
+  );
+
+export const createTemporaryTable = async (
+  tableInput: AWS.DynamoDB.CreateTableInput
+): Promise<void> =>
+  new Promise((resolve, reject) =>
+    getDynamoDB('local').createTable(tableInput, (err, {TableDescription}) => {
+      if (!TableDescription || err) {
+        console.error(err);
+        return reject(new Error('Failed to create table'));
+      }
+      console.log('Created temporary table', TableDescription.TableName);
+      return resolve();
+    })
+  );
+
+export const getItemsFromSourceTable = async (
+  itemStream: Transform,
+  totalCount = 0,
+  startKey: Key | undefined = undefined
+): Promise<void> =>
+  new Promise((resolve: (count: number) => void, reject) =>
+    getDynamoDB('source').scan(
+      {
+        TableName: tableName,
+        ExclusiveStartKey: startKey,
+        ConsistentRead: true,
+      },
+      (err, {Items, Count, LastEvaluatedKey}) => {
+        if (err) {
+          return reject(new Error('Failed to get items from table'));
+        }
+
+        if (!Items || !Count) {
+          return reject(new Error('Table did not contain any items'));
+        }
+
+        // Add fetched items to stream
+        itemStream.push(Items);
+
+        if (LastEvaluatedKey !== undefined) {
+          return getItemsFromSourceTable(
+            itemStream,
+            totalCount + Count,
+            LastEvaluatedKey
+          );
+        } else {
+          return resolve(totalCount + Count);
+        }
+      }
+    )
+  )
+    .then(totalCount => {
+      console.log('Scanned table', tableName, '(' + totalCount, 'items)');
+    })
+    .catch(e => {
+      console.log('Failed to scan table', tableName);
+      itemStream.write(e);
+    })
+    .finally(() => {
+      itemStream.end();
+    });
+
+/*
+const copyItemsToTemporaryTable = async (
+  items: DynamoDB.AttributeMap[]
+): Promise<void> => {
+  return new Promise((resolve, reject) => {
+      return getDynamoDB('local').transactWriteItems(,
+    return resolve();
+  });
+};
+*/
